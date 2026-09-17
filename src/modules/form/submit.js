3aa2e44aad86d1f8fa54f69679208560bcc7e55b
vitorcosta <vitorsfc26@gmail.com>
2026-09-17 17:02:15 -0300

    dates and hours

---
+++ src/modules/form/submit.js
@@ -0,0 +1,40 @@
+import dayjs from "dayjs"
+
+const form = document.querySelector("form")
+const selectedDate = document.getElementById("date")
+const clientName = document.getElementById("client")
+
+selectedDate.value = dayjs(new Date()).format("YYYY-MM-DD")
+selectedDate.min = dayjs(new Date()).format("YYYY-MM-DD")
+
+form.onsubmit = (event) => {
+    event.preventDefault()
+
+    try {
+        const name = clientName.value.trim()
+
+        if (!name) {
+            return alert("Informe o nome do cliente!")
+        }
+
+        const hourSelected = document.querySelector(".hour-selected")
+
+        if (!hourSelected) {
+            return alert("Selecione a hora.")
+        }
+
+        const [hour] = hourSelected.innerText.split(":")
+
+        const when = dayjs(selectedDate.value).add(hour, "hour")
+
+        const id = new Date().getTime()
+
+        console.log({
+            id, name, when
+        })
+        
+    } catch (error) {
+        alert("Não foi possível realizar o agendamento.")
+        console.log(error)
+    }
+}
